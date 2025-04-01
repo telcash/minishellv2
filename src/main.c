3173216f4f61fca7c522fb18a083b4c9c2e4e02d
Carlos Salazar <csalazar@c2r18s5.42madrid.com>
2025-04-01 15:36:45 +0200

    Signals

--- src/main.c
+++ src/main.c
@@ -3,47 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: carlossalazar <carlossalazar@student.42    +#+  +:+       +#+        */
+/*   By: csalazar <csalazar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 08:38:43 by carlossalaz       #+#    #+#             */
-/*   Updated: 2025/03/30 09:57:21 by carlossalaz      ###   ########.fr       */
+/*   Updated: 2025/04/01 15:35:48 by csalazar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static void verify_input(int argc, char **argv)
+static void	verify_input(int argc, char **argv)
 {
-    if (argc && argv)
-        return ;
-    else
-        ft_putstr_fd("Error: invalid input\n", 2);
-    exit(1);
+	if (argc && argv)
+		return ;
+	else
+		ft_putstr_fd("Error: invalid input\n", 2);
+	exit(1);
 }
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    t_shell *shell;
-    t_token **token;
-    char *line;
+	t_shell	*shell;
+	t_token	**token;
+	char	*line;
 
-    verify_input(argc, argv);
-    init_minishell(&shell, envp);
-    set_signal();
-    while (1)
-    {
-        line = readline("minishell> ");
-        if (!line)
-            break ;
-        if (line && *line)
-        {
-            add_history(line);
-            token = get_token(line, shell);
-            pipeline(token, shell);
-            free(line);
-            free_token(token);
-        }
-    }
-    free_shell(shell);
-    return (0);
+	verify_input(argc, argv);
+	init_minishell(&shell, envp);
+	set_signal();
+	while (1)
+	{
+		line = readline("minishell> ");
+		if (!line)
+		{
+			ft_putendl_fd("exit", 2);
+			exit(0);
+		}
+		if (line && *line)
+		{
+			add_history(line);
+			token = get_token(line, shell);
+			pipeline(token, shell);
+			free(line);
+			free_token(token);
+		}
+	}
+	return (free_shell(shell), 0);
 }
